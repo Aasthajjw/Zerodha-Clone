6109cddcc3bc77694d5a0ca8693a991c5680cc23
Aasthajjw <aasthajjw31@gmail.com>
2025-07-01 15:26:04 +0530

    Update App.js

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,8 +1,8 @@
 import {Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage.js";
-import Login from "./components/Login.jsx";
-import Signup from "./components/Signup.jsx";
-import Home from "./components/Home.jsx";
+import Login from "./components/Login.js";
+import Signup from "./components/Signup.js";
+import Home from "./components/Home.js";
 
 function App() {
   return (
