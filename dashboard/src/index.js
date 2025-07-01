6ae0ee91f421eebbe80fae7af8323799d2a65e0f
Aasthajjw <aasthajjw31@gmail.com>
2025-07-01 14:55:00 +0530

    Update index.js

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import './index.css'
 import HomePage from "./components/HomePage";
@@ -9,10 +9,10 @@ import { CookiesProvider } from "react-cookie";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <CookiesProvider>
         <App />
       </CookiesProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
