be4ec3dfa72e8c9526451db9c7ea656230715538
Aasthajjw <aasthajjw31@gmail.com>
2025-07-01 13:35:39 +0530

    Update Signup.jsx

--- dashboard/src/components/Signup.jsx
+++ dashboard/src/components/Signup.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://zerodha-clone-backend-805t.onrender.com/signup",
+        "http://localhost:3000/signup",
         {
           ...inputValue,
         },
